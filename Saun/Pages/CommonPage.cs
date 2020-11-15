ea6d9ebc4017815b520b2a30465ea088c5866020
Johann Kuldmäe <jokuld@ttu.ee>
2020-11-15 11:58:13 +0200

    Fixed CommonPage

---
+++ Saun/Pages/CommonPage.cs
@@ -0,0 +1,43 @@
+﻿using Data.Abstractions;
+using Domain.Abstractions;
+using Facade.Abstractions;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pages
+{
+    public abstract class CommonPage<TRepository, TDomain, TView, TData> :
+        BasePage<TRepository, TDomain, TView, TData>
+        where TRepository : ICrudMethods<TDomain>, ISorting, IFiltering, IPaging
+        where TView : UniqueEntityView
+    {
+
+        protected internal CommonPage(TRepository r) : base(r) { }
+
+        public abstract string ItemId { get; }
+
+        public string PageTitle { get; set; }
+
+        public string PageSubTitle => GetPageSubTitle();
+
+        protected internal virtual string GetPageSubTitle() => string.Empty;
+
+        public string PageUrl => GetPageUrl();
+
+        protected internal abstract string GetPageUrl();
+
+        public string IndexUrl => GetIndexUrl();
+
+        protected internal string GetIndexUrl() => $"{PageUrl}/Index?fixedFilter={FixedFilter}&fixedValue={FixedValue}";
+
+        protected static IEnumerable<SelectListItem> CreateSelectList<TTDomain, TTData>(IRepository<TTDomain> r)
+            where TTDomain : UniqueEntity<TTData>
+            where TTData : NamedEntityData, new()
+        {
+            var items = r.Get().GetAwaiter().GetResult();
+
+            return items.Select(m => new SelectListItem(m.Data.Name, m.Data.Id.ToString("B"))).ToList();
+        }
+    }
+}
