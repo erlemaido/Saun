596be2ae5495831e819469e87cfe5dbaf9c0d000
Johann Kuldmäe <jokuld@ttu.ee>
2020-11-15 12:43:01 +0200

    Added CatalogItemBrands page

---
+++ Saun/Pages/CatalogItemBrands/CatalogItemBrandsPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Brands;
+using Domain.Brands;
+using Facade.Brands;
+
+namespace Pages.Brands
+{
+    class BrandsPage : CommonPage<IBrandsRepository, Brand, BrandView, BrandData>
+    {
+        protected internal BrandsPage(IBrandsRepository BrandsRepository) : base(
+            BrandsRepository)
+        {
+            PageTitle = "Toodete brändid";
+        }
+
+        public override Guid ItemId => Item.Id;
+
+        protected internal override string GetPageUrl() => "/Saun/Brands";
+       
+        protected internal override Brand ToObject(BrandView view)
+        {
+            return BrandViewFactory.Create(view);
+        }
+
+        protected internal override BrandView ToView(Brand obj)
+        {
+            return BrandViewFactory.Create(obj);
+        }
+    }
+}
