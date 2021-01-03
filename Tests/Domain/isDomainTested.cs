da91daf7c867f2e2885fbf55b809460a675f7bee
johannkuldm2e <38594359+johannkuldm2e@users.noreply.github.com>
2021-01-03 17:28:49 +0200

    Added isDomainTested

---
+++ Tests/Domain/isDomainTested.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tests;
+
+namespace TrainingApp.Tests.Domain
+{
+    [TestClass]
+    public class IsDomainTested : AssemblyTests
+    {
+        private const string Assembly = "Domain";
+
+        protected override string Namespace(string name) { return $"{Assembly}.{name}"; }
+
+        [TestMethod] public void IsAbstractionsTested() { IsAllTested(Assembly, Namespace("Abstractions")); }
+        [TestMethod] public void IsShopTested() { IsAllTested(Assembly, Namespace("Shop")); }
+    }
+}
