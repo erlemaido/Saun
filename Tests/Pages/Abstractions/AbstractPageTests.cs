1b44814bffd475428146fa5de35094f48b21b416
johannkuldm2e <38594359+johannkuldm2e@users.noreply.github.com>
2021-01-08 16:56:41 +0200

    tests

--- Tests/Pages/Abstractions/AbstractPageTests.cs
+++ Tests/Pages/Abstractions/AbstractPageTests.cs
@@ -26,7 +26,7 @@ namespace Tests.Pages.Abstractions {
 
             protected internal override string GetPageSubtitle() => SubTitle;
 
-            
+             
         }
 
         internal class TestRepository : BaseTestRepositoryForUniqueEntity<Order, OrderData>, IOrdersRepository
