e7dda9499bd3d564f6db6776e3683dc3833b4664
Erle Maido <maido.erle@gmail.com>
2021-01-09 18:05:33 +0200

    fixed actions captions names

--- Saun/Pages/Abstractions/Constants/Actions.cs
+++ Saun/Pages/Abstractions/Constants/Actions.cs
@@ -2,11 +2,11 @@ namespace Sauna.Pages.Abstractions.Constants
 {
     public static class Actions {
 
-        public static string Edit => "Muuda";
-        public static string Details => "Detailid";
-        public static string Delete => "Kustuta";
+        public static string Edit => "Edit";
+        public static string Details => "Details";
+        public static string Delete => "Delete";
         public static string Index => "Index";
-        public static string AddToBasket => "Lisa ostukorvi";
+        public static string AddToBasket => "AddToCart";
 
     }
 
