1ee14c05622d623a43fe85d4aa3ae583c6f22e75
Erle Maido <maido.erle@gmail.com>
2021-01-10 00:05:26 +0200

    added layouts

--- Saun/Infra/Migrations/20210109214933_InitialDbCreation.cs
+++ Saun/Infra/Migrations/20210109214933_InitialDbCreation.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Infra.Migrations
 {
-    public partial class initial : Migration
+    public partial class InitialDbCreation : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
