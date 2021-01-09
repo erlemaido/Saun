ccce09178c53b7d64aa1853b30f6162014ffc1ea
Erle Maido <47369398+erlemaido@users.noreply.github.com>
2021-01-10 00:20:10 +0200

    esileht (#85)
    Co-authored-by: erlemaido <Mustikasmustikas1>

--- Saun/WebApp/Pages/Index.cshtml.cs
+++ Saun/WebApp/Pages/Index.cshtml.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Shop.Products;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace WebApp.Pages
@@ -6,15 +10,21 @@ namespace WebApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public IEnumerable<Product> Products;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(
+            IProductsRepository repository,
+            ILogger<IndexModel> logger)
         {
+            Products = repository.Get().Result.ToList();
             _logger = logger;
         }
 
         public void OnGet()
         {
-
+            var rnd = new Random();
+            var randomList = Products.OrderBy(x => rnd.Next()).Take(6);
+            Products = randomList;
         }
     }
 }
