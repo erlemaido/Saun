81cd9c241aaedc17e956112923ac3bdf2c4bc29c
Erle Maido <47369398+erlemaido@users.noreply.github.com>
2021-01-10 14:43:37 +0200

    Class diagrams (#92)
    * added diagrams
    
    * fixed user and unitdata

--- Saun/Domain/Abstractions/NamedEntity.cs
+++ Saun/Domain/Abstractions/NamedEntity.cs
@@ -2,7 +2,7 @@ using Data.Abstractions;
 
 namespace Domain.Abstractions
 {
-    public abstract class NamedEntity<TData> : UniqueEntity<TData> where TData : NamedEntityData, new()
+    public abstract class NamedEntity<TData> : UniqueEntity<TData>, INamedEntity<TData> where TData : NamedEntityData, new()
     {
         protected internal NamedEntity(TData data) : base(data)
         {
