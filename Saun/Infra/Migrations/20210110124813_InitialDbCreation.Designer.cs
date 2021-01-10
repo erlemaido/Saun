0eaf7be9c189b77451a8f1604cc832728df1a4d4
Erle Maido <maido.erle@gmail.com>
2021-01-10 14:49:53 +0200

    added migration

--- Saun/Infra/Migrations/20210110124813_InitialDbCreation.Designer.cs
+++ Saun/Infra/Migrations/20210110124813_InitialDbCreation.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infra.Migrations
 {
     [DbContext(typeof(SaunaDbContext))]
-    [Migration("20210110111011_InitialDbCreation")]
+    [Migration("20210110124813_InitialDbCreation")]
     partial class InitialDbCreation
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
